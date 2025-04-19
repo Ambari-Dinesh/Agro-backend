cd82b0e0be3f3c00653f963fa406eaf60d43dafa
Ambari-Dinesh <ambaridinesh@gmail.com>
2025-04-19 18:49:14 +0530

    Updated bcrypt to bcryptjs to resolve Vercel deployment issue

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
+
 const db = require("../db"); // assuming you're using pg or knex
 const jwt = require("jsonwebtoken");
 
